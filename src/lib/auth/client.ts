e42e39bc74cbf59f57a4e0c2f481fe6619d6d640
Yazal Ulloa <yazalulloa@gmail.com>
2025-09-04 14:03:58 -0400

    rollback working commit

--- src/lib/auth/client.ts
+++ src/lib/auth/client.ts
@@ -1,9 +1,9 @@
 import { createAuthClient } from 'better-auth/react'
-import { adminClient, multiSessionClient } from 'better-auth/client/plugins'
+import { adminClient } from 'better-auth/client/plugins'
 
 export const authClient = createAuthClient({
   baseURL: `${process.env.NEXT_PUBLIC_SERVER_URL}`,
-  plugins: [adminClient(), multiSessionClient()],
+  plugins: [adminClient()],
   fetchOptions: {
     onError(e) {
       if (e.error.status === 429) {
